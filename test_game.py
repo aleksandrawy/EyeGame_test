4d3d8523c886f8ae6fef117b63db65a8734056d6
Aleksandra <aleksandraw@vp.pl>
2017-07-07 15:49:53 +0200

    More tests.

--- test_game.py
+++ test_game.py
@@ -9,12 +9,17 @@ Level = namedtuple("Level", ['name', 'value'])
 JASTRZAB = Level(name='JASTRZĄB', value=25)
 ROBOT = Level(name='ROBOT', value=30)
 
+# class MyLevel:
+#
+#     def __init__(self, name, value):
+#         self.name = name
+#         self.value = value
+
 # def test_exception():
 #     assert False
 
 @pytest.mark.parametrize("level", [JASTRZAB, ROBOT])
 def test_levels(driver, level):
-    #driver = get_driver()
     eye_game = EyeGamePage(driver)
     eye_game.load()
     eye_game.get_to_level(level=level)
@@ -22,7 +27,6 @@ def test_levels(driver, level):
 
 @pytest.mark.smoke_test
 def test_title(driver):
-
     eye_game = EyeGamePage(driver)
     eye_game.load()
     assert "Eye" in eye_game.get_title()
