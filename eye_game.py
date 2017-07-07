a975456b6898bdcffbf97d527a65faebbf173f5c
Aleksandra <aleksandraw@vp.pl>
2017-07-07 13:04:06 +0200

    Levels changed.

--- eye_game.py
+++ eye_game.py
@@ -2,12 +2,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from collections import namedtuple
 from utils import wait_until
 
+Level = namedtuple("Level", ['name', 'value'])
 
 class EyeGamePage:
 
     url = "https://www.igame.com/eye-test/"
+
+    JASTRZAB = Level(name='jastrząb', value=25)
+    ROBOT = Level(name='robot', value=30)
+    #ROBOT.name / ROBOT.value
+
+    # class Levels(Enum):
+    #     ROBOT = 30
+    #     JASTRZAB = 25
+
+
     def __init__(self, driver : WebDriver):   #klasa oczekuje ze dostanie instancje drivera
         self.driver = driver
 
@@ -25,12 +37,19 @@ class EyeGamePage:
         self.driver.find_element_by_css_selector('.clock').text
 
 
-    def get_to_robot_level(self):
-        for i in range(30):
+    def get_to_level(self, num_of_clicks):
+        for i in range(num_of_clicks):
             self.click_chosen_one()
-
-       # wait_until(condition=)
         WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.ID, "timeleft")))
+
+
+    def get_to_robot_level(self):
+        self.get_to_level(self.ROBOT.value)
+       #  for i in range(30):
+       #      self.click_chosen_one()
+       # # wait_until(condition=)
+       #  WebDriverWait(self.driver, 20).until(EC.invisibility_of_element_located((By.ID, "timeleft")))
+
 
     def get_reached_level(self):
         return self.driver.find_element(By.CSS_SELECTOR, '.character-title').text
